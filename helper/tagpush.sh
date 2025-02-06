de9c2bb6ce402a0e995a58e893ae7458a59fa0c2
Platy <daryl.limkz@gmail.com>
2025-02-06 12:55:28 +0800

    feat: auto delete tags after push
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- helper/tagpush.sh
+++ helper/tagpush.sh
@@ -6,10 +6,16 @@ if [ "$(uname -m)" != "x86_64" ]; then
 
     docker push platypew/pwnpad:lite-arm64
     docker push platypew/pwnpad:latest-arm64
+
+    docker rmi platypew/pwnpad:lite-arm64
+    docker rmi platypew/pwnpad:latest-arm64
 else
     docker tag platypew/pwnpad:lite platypew/pwnpad:lite-amd64
     docker tag platypew/pwnpad:latest platypew/pwnpad:latest-amd64
 
     docker push platypew/pwnpad:lite-amd64
     docker push platypew/pwnpad:latest-amd64
+
+    docker rmi platypew/pwnpad:lite-amd64
+    docker rmi platypew/pwnpad:latest-amd64
 fi
